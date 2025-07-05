e7c2f076ca63fe88d7b0a766f3363d7b862e3e5b
unknown <ilznorz582@gmail.com>
2025-07-05 17:22:47 +0700

    fix: endpoint get me not found

--- src/hooks/useUserQuery.tsx
+++ src/hooks/useUserQuery.tsx
@@ -1,22 +1,15 @@
 import api from "@/lib/api";
 import { ApiResponse } from "@/types/api";
-import { getId } from "@/lib/cookie";
 import { User } from "@/types/user";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useUserQuery() {
-    const id =
-        typeof window !== "undefined" && typeof localStorage !== "undefined"
-            ? getId()
-            : undefined;
-
     const query = useQuery<ApiResponse<User>>({
         queryKey: ["user"],
         queryFn: async () => {
-            const response = await api.get(`/user/me/${id}`);
+            const response = await api.get(`/user/me`);
             return response.data;
         },
-        enabled: !!id,
         refetchOnWindowFocus: false,
     });
 
